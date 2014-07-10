ec2a9ca50b527169304f8bc9da3886bf3f8871ec
Pei <peitao@mit.edu>
2014-07-10 14:15:56 -0400

    rudimentary highlighting

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-
+var colors=['LightYellow', 'LightBlue', 'LightPink', 'LightGreen', 'Plum', 'LightSalmon'];
+var highlightCount=0;
 
 $(document).ready(function(){
-    
-    
-})
+   $('.poemLine').each(function(){
+       var check =$('<input type="checkbox" class="rhymeCheck">');
+       $(this).prepend(check)
+   });
+   
+   $('#rhymeHighlight').on('click',function(){
+       var checkboxes=$('.rhymeCheck');
+       var checked=[];
+       checkboxes.each(function(){
+        if (this.checked === true){
+            checked.push(this);
+        }
+       });
+       
+       checked.forEach(function(c){
+           $($(c).parent()).css('background-color', colors[highlightCount]);
+       })
+       highlightCount++;
+       
+   });
+});
 
