6eebc1d4e8e08a43b2c45cc7ac0384a1d082c3e8
Pei <peitao@mit.edu>
2014-07-10 14:36:05 -0400

    toggle based highlighting enacted

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var colors=['LightYellow', 'LightBlue', 'LightPink', 'LightGreen', 'Plum', 'LightSalmon'];
+var colors=['Yellow', 'LightBlue', 'LightPink', 'LightGreen', 'Plum', 'LightSalmon'];
 var highlightCount=0;
 
 $(document).ready(function(){
@@ -6,20 +6,25 @@ $(document).ready(function(){
        var check =$('<input type="checkbox" class="rhymeCheck">');
        $(this).prepend(check)
    });
+
    
-   $('#rhymeHighlight').on('click',function(){
+   $('.rhymeHighlight').on('click',function(){
        var checkboxes=$('.rhymeCheck');
        var checked=[];
        checkboxes.each(function(){
         if (this.checked === true){
             checked.push(this);
+            this.checked = false;
         }
        });
        
        checked.forEach(function(c){
-           $($(c).parent()).css('background-color', colors[highlightCount]);
-       })
+           $($(c).parent()).find('span').css('background-color', colors[highlightCount]);
+       });
        highlightCount++;
+   });
+   
+   $('.syllablesOption').on('click',function(){
        
    });
 });
