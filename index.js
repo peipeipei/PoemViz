75a0ca340e8f5215b4c84e9965cff824ebc02091
Pei <peitao@mit.edu>
2014-07-11 11:49:14 -0400

    line syllables added and counts up but huge bug with the count integrating into the line within a minute and recounting from zero also formatting progress

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var colors=['Yellow', 'LightBlue', 'LightPink', 'LightGreen', 'Plum', 'LightSalm
 var highlightColor='';
 var highlighting=true;
 var selectedLayer='';
+var highlightElement='line';
 
 $(document).ready(function(){
     
     //make each alphanumeric character/space/word/line a span with seperate class
     function makeSpans() {
-        var count = 0;
         $('.poemLine').each(function(){
             console.log('hi');
             var elements = $(this).text().split('');
@@ -26,32 +26,65 @@ $(document).ready(function(){
                 $(word).append('<span class=letter>' + elements[i] + '</span>');
                 }
             }
+            var lineCount = $('<span class=lineCount style="color:blue; font-weight:bold">');
+            $(this).append(lineCount);
         });
     }
     
     makeSpans();
+    
+    function initialCount(){
+        $('.poemLine').each(function(){
+            var words = $(this).find('.word');
+            var wordCount = words.length;
+            var lineCount = $(this).children('.lineCount')[0]
+            $(lineCount).text(wordCount);
+        })
+    }
+    
+    initialCount();
 
 //   $('.rhymeHighlight').on('click',function(){
 //       if (highlighting){
 //         $('.highlightColors').css('display', 'none');
 //           highlighting=false;
-//           $('.rhymeHighlight').text('Start Hightlighting');
+//           $('.rhymeHighlight').text('Start Highlighting');
 //       }
 //       else{
 //         $('.highlightColors').css('display', 'inline-block');
 //         highlighting=true;
-//         $('.rhymeHighlight').text('Stop Hightlighting');
+//         $('.rhymeHighlight').text('Stop Highlighting');
 
 //       }
 //   });
    
-   colors.forEach(function(c){
+   $('.layer').on('click',function(){
+       var clickedLayer=this;
+      $('.layer').each(function(){
+          if(this == clickedLayer){
+              $(this).css('opacity', 1.0);
+               selectedLayer=$(this).data('name');
+               console.log(selectedLayer);
+          }else{
+              $(this).css('opacity',0.5);
+          }
+      }); 
+   });
+   
+/*   colors.forEach(function(c){
        var colorSquare=$('<div class="colorSquare"></div>');
        colorSquare.data('color', c);
        colorSquare.css('background-color', c);
        $('.highlightColors').append(colorSquare);
        
-   })
+   })*/
+   
+   for (i=0;i<2;i++){
+       var colorSquare=$('<div class="colorSquare"></div>');
+       colorSquare.data('color', colors[i]);
+       colorSquare.css('background-color', colors[i]);
+       $('.highlightColors').append(colorSquare);
+   }
     
     $('.colorSquare').on('click',function(){
         highlightColor=$(this).data('color');
@@ -63,11 +96,20 @@ $(document).ready(function(){
     });
     
 
-    $('.poemLine').on('click', function(){
-        if(highlighting){
-            //console.log(this.firstElementChild);
-            $(event.target).css('background-color',highlightColor);
+    $('.line').on('click', function(){
+        if(selectedLayer=='rhyme' && highlightElement=='line'){
+            $(this).css('background-color',highlightColor);
         }
+    })
+    
+    $('.word').on('click', function(){
+        if(selectedLayer=='rhyme' && highlightElement=='word'){
+            $(this).css('background-color',highlightColor);
+        }
+    })
+    
+    $('#rhymeSelect').on('change',function(){
+      highlightElement=$('#rhymeSelect').val();
     })
     
     function addSyllable(range){
@@ -75,17 +117,21 @@ $(document).ready(function(){
         verticalLine.css('font-weight', 'bold');
         verticalLine.css('color', 'red');
         verticalLine.css('font-size', '1.3em');
+        var countSpan = $(range.commonAncestorContainer.parentElement).parent().parent().parent().children('.lineCount')[0];
+        var count = $(countSpan).text();
+        count++;
+        $(countSpan).text(count);
         $(range.commonAncestorContainer.parentElement).append(verticalLine);
     }
    
-$('.syllablesOption').on('click',function(){
-    $('.poem').click(function (e) { //Default mouse Position
-        var sel = window.getSelection();
-        var range = sel.getRangeAt(0);
-
-        addSyllable(range);
+    $('.syllablesOption').on('click',function(){
+        $('.poem').click(function (e) { //Default mouse Position
+            var sel = window.getSelection();
+            var range = sel.getRangeAt(0);
+    
+            addSyllable(range);
+        });
     });
-});
   
     $('.syllablesCount').on('click', function(){
         console.log('click');
