1d5221f2c9bfbabbcbc3f15cd84fb5e0be43ed25
LeighMarie <leighmariebraswell@31-33-185.wireless.csail.mit.edu>
2014-08-08 09:20:07 -0400

    merge conflict

--- client/textOptions.js
+++ client/textOptions.js
@@ -45,7 +45,27 @@ Template.poem.events({
 //change how poem lines are displayed
 'change .optionSelect':function(event){
     Session.set('breaksOption',$(event.currentTarget).val());
-    //setTimeout(function() {syllableCounts()}, 1000);
+    /*if (Session.get('breaksOption') == 'origOption'){
+    }
+    if (Session.get('breaksOption') == 'puncOption'){
+        $('#poemWrapper').html("<div class = 'poemIdentifiers' id = 'poemTitle'><br>{{poemTitle}}</div>"+
+               "<div class = 'poemIdentifiers' id = 'poemAuthor'>By {{poemAuthor}}</div>"+
+                "<div class = 'syllableCount'>Syllable Count</div>"+
+                    "{{# each lines1}}"+
+                    "<span class={{this.class}} id={{this.line_id}}>"+    
+                    "{{# each this.content}}"+
+                    "<span id={{this.word_id}} class = {{this.class}}>"+
+        "<span class='syllable'>{{# each this.content}}<span id={{this.letter_id}}  class={{this.class}}>{{this.content}}</span> {{/each}}</span>"+
+                            "<span class = 'space'>&nbsp;</span>"+
+                        "</span>"+
+                   "{{/each}}"+
+                   "<span class='lineCount'></span>"+
+                    "</span>"+
+                    "<br>"+
+                    "{{/each}}");
+    }
+    if (Session.get('breaksOption') == 'sentOption'){
+    }*/
 }
 });
   
